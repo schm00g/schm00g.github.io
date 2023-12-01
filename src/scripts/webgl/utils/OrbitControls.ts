465450e8aaf03f66e21eb6aa821d538646347a42
schm00g <hk8igureu@relay.firefox.com>
2023-12-01 20:21:43 +0000

    fix orbit controls min max zoom

--- src/scripts/webgl/utils/OrbitControls.ts
+++ src/scripts/webgl/utils/OrbitControls.ts
@@ -1,26 +1,28 @@
-import { OrbitControls as OC } from 'three/examples/jsm/controls/OrbitControls'
-import { gl } from '../core/WebGL'
+import { OrbitControls as OC } from "three/examples/jsm/controls/OrbitControls";
+import { gl } from "../core/WebGL";
 
 class OrbitControls {
-  private orbitControls: OC
+  private orbitControls: OC;
 
   constructor() {
-    this.orbitControls = new OC(gl.camera, gl.renderer.domElement)
-    this.orbitControls.enableDamping = true
-    this.orbitControls.dampingFactor = 0.1
+    this.orbitControls = new OC(gl.camera, gl.renderer.domElement);
+    this.orbitControls.minDistance = 300;
+    this.orbitControls.maxDistance = 700;
+    this.orbitControls.enableDamping = true;
+    this.orbitControls.dampingFactor = 0.1;
   }
 
   get primitive() {
-    return this.orbitControls
+    return this.orbitControls;
   }
 
   disableDamping() {
-    this.orbitControls.enableDamping = false
+    this.orbitControls.enableDamping = false;
   }
 
   update() {
-    this.orbitControls.update()
+    this.orbitControls.update();
   }
 }
 
-export const controls = new OrbitControls()
+export const controls = new OrbitControls();
